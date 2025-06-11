a9b6d40cef920a5552ac9808a5a2115977e14dfa
hyeonjune <annhj980@naver.com>
2025-06-12 01:29:51 +0900

    [TEST] TemporaryUserServiceImpl 테스트 구현
    - 임시 사용자 저장
    - 임시 사용자 이메일 중복시 예외 처리
    - 임시 사용자 아이디 확인
    - 임시 사용자 이메일 중복 확인
    - 임시 사용자 이메일 삭제
    - 기준 시간 이전의 임시 사용자 조회

---
+++ jejuday/src/test/java/com/goodda/jejuday/Auth/service/impl/TemporaryUserServiceImplTest.java
@@ -0,0 +1,124 @@
+package com.goodda.jejuday.Auth.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.goodda.jejuday.Auth.entity.Language;
+import com.goodda.jejuday.Auth.entity.Platform;
+import com.goodda.jejuday.Auth.entity.TemporaryUser;
+import com.goodda.jejuday.Auth.repository.TemporaryUserRepository;
+import com.goodda.jejuday.Auth.util.exception.DuplicateEmailException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+class TemporaryUserServiceImplTest {
+
+    private TemporaryUserRepository temporaryUserRepository;
+    private TemporaryUserServiceImpl temporaryUserService;
+
+    @BeforeEach
+    void setUp() {
+        temporaryUserRepository = mock(TemporaryUserRepository.class);
+        temporaryUserService = new TemporaryUserServiceImpl(temporaryUserRepository);
+    }
+
+    @Test
+    void 임시저장_테스트() {
+        //  given
+        String name = "tester";
+        String email = "test@naver.com";
+        String password = "testpassword";
+        Language language = Language.KOREAN;
+        Platform platform = Platform.APP;
+
+        //  when
+        temporaryUserService.save(name, email, password, language, platform);
+
+        //  then
+        ArgumentCaptor<TemporaryUser> captor = ArgumentCaptor.forClass(TemporaryUser.class);
+        verify(temporaryUserRepository, times(1)).save(captor.capture());
+
+        TemporaryUser temporaryUser = captor.getValue();
+
+        assertEquals(name, temporaryUser.getName());
+        assertEquals(email, temporaryUser.getEmail());
+        assertEquals(password, temporaryUser.getPassword());
+    }
+
+    @Test
+    @DisplayName("이메일 중복으로 인한 예외")
+    void 임시저장_예외() {
+        //  given
+        String name = "tester";
+        String email = "test@naver.com";
+        String password = "testpassword";
+        Language language = Language.KOREAN;
+        Platform platform = Platform.APP;
+
+        //  when
+        when(temporaryUserRepository.existsByEmail(email)).thenReturn(true);
+
+        //  then
+        DuplicateEmailException exception = assertThrows(DuplicateEmailException.class,
+                () -> temporaryUserService.save(name, email, password, language, platform));
+
+        assertEquals("이미 존재하는 이메일입니다.", exception.getMessage());
+
+        verify(temporaryUserRepository, times(0)).save(any());
+    }
+
+    @Test
+    void 이메일_찾기() {
+        //  given
+        String email = "test@naver.com";
+        TemporaryUser temporaryUser = TemporaryUser.builder().email(email).build();
+        when(temporaryUserRepository.findByEmail(email)).thenReturn(Optional.of(temporaryUser));
+
+        //  when
+        Optional<TemporaryUser> result = temporaryUserService.findByEmail(email);
+
+        //  then
+        assertTrue(result.isPresent());
+        assertEquals(email, result.get().getEmail());
+    }
+
+    @Test
+    void 이메일_중복여부() {
+        String email = "test@naver.com";
+        when(temporaryUserRepository.existsByEmail(email)).thenReturn(true);
+        assertTrue(temporaryUserService.existsByEmail(email));
+    }
+
+    @Test
+    void 이메일_삭제() {
+        Long id = 12L;
+        temporaryUserService.deleteByTemporaryUserId(id);
+        verify(temporaryUserRepository).deleteByTemporaryUserId(id);
+    }
+
+    @Test
+    void 시간보다_이전의_임시사용자_조회() {
+        // given
+        LocalDateTime cutoff = LocalDateTime.of(2025, 1, 1, 12, 0);  // 고정된 시간
+        TemporaryUser mockUser = TemporaryUser.builder().email("old@test.com").build();
+        when(temporaryUserRepository.findByCreatedAtBefore(cutoff)).thenReturn(List.of(mockUser));
+
+        // when
+        List<TemporaryUser> result = temporaryUserService.findByCreatedAtBefore(cutoff);
+
+        // then
+        assertEquals(1, result.size());
+        assertEquals("old@test.com", result.get(0).getEmail());
+    }
+}
