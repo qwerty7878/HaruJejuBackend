68165079ce2c4e9cb4080e39a9d95e373c6c7670
hyeonjune <annhj980@naver.com>
2025-06-13 01:24:01 +0900

    [FEAT] JWT 구현

---
+++ jejuday/src/main/java/com/goodda/jejuday/Auth/security/CookieUtil.java
@@ -0,0 +1,45 @@
+package com.goodda.jejuday.Auth.security;
+
+import com.goodda.jejuday.Auth.entity.CookieRule;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class CookieUtil {
+
+    public String resolveTokenFromCookie(Cookie[] cookies, CookieRule cookieRule) {
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(cookieRule.getValue()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse("");
+    }
+
+    public void addJwtCookie(HttpServletResponse response, String name, String token, boolean secure) {
+        ResponseCookie cookie = ResponseCookie.from(name, token)
+                .httpOnly(true)
+                .secure(secure)
+                .path("/")
+                .sameSite("None")
+                .maxAge(86400) // 1일
+                .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+
+    public void clearJwtCookie(HttpServletResponse response, String name, boolean secure) {
+        ResponseCookie cookie = ResponseCookie.from(name, "")
+                .httpOnly(true)
+                .secure(secure)
+                .path("/")
+                .sameSite("None")
+                .maxAge(0)
+                .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+}
