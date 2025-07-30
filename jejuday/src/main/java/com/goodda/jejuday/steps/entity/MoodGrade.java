316fdfefdbb76407a2ad87c53c546deb5eef1ce2
hyeonjune <annhj980@naver.com>
2025-07-31 01:38:26 +0900

    [CHORE] 언어 삭제 및 등급별 이름 수정

--- jejuday/src/main/java/com/goodda/jejuday/steps/entity/MoodGrade.java
+++ jejuday/src/main/java/com/goodda/jejuday/steps/entity/MoodGrade.java
@@ -1,11 +1,11 @@
 package com.goodda.jejuday.steps.entity;
 
 public enum MoodGrade {
-    VERY_SAD(0),
-    SAD(50),
-    NORMAL(100),
-    HAPPY(200),
-    VERY_HAPPY(300);
+    BALBADAK(0),    //  발바닥
+    DDUBUCKI(50),   //  뚜벅이
+    OREUMKKUN(50),  //  오름꾼
+    HALLAMAN(50),   //  한라맨
+    TAMLAWANG(50);  //  탐라왕
 
     private final int reward;
 
@@ -18,11 +18,11 @@ public enum MoodGrade {
     }
 
     public static MoodGrade fromSteps(long totalSteps) {
-        if (totalSteps < 10_000) return VERY_SAD;
-        else if (totalSteps < 50_000) return SAD;
-        else if (totalSteps < 100_000) return NORMAL;
-        else if (totalSteps < 300_000) return HAPPY;
-        else return VERY_HAPPY;
+        if (totalSteps < 20_000) return BALBADAK;
+        else if (totalSteps < 40_000) return DDUBUCKI;
+        else if (totalSteps < 60_000) return OREUMKKUN;
+        else if (totalSteps < 80_000) return HALLAMAN;
+        else return TAMLAWANG;
     }
 }
 
