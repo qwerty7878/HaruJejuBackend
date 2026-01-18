48c4b5586209ef6bee6306abd754b5a9abd62223
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-01-19 08:46:12 +0900

    Clean up imports and remove EntityScan annotation
    Removed unused imports and EntityScan annotation.

--- jejuday/src/main/java/com/goodda/jejuday/JejudayApplication.java
+++ jejuday/src/main/java/com/goodda/jejuday/JejudayApplication.java
@@ -3,14 +3,9 @@ package com.goodda.jejuday;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import com.goodda.jejuday.auth.entity.User;
-import com.goodda.jejuday.spot.entity.Spot;
-import com.goodda.jejuday.spot.entity.SpotViewLog;
 
 @EnableScheduling
 @SpringBootApplication
-@EntityScan(basePackageClasses={User.class, Spot.class, SpotViewLog.class})
 public class JejudayApplication {
 
     public static void main(String[] args) {
