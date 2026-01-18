596d7176df84b69ad8602dd06527c51ad735d615
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-01-19 08:37:56 +0900

    Add entity scanning for User, Spot, and SpotViewLog

--- jejuday/src/main/java/com/goodda/jejuday/JejudayApplication.java
+++ jejuday/src/main/java/com/goodda/jejuday/JejudayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import com.goodda.jejuday.auth.entity.User;
+import com.goodda.jejuday.spot.entity.Spot;
+import com.goodda.jejuday.spot.entity.SpotViewLog;
 
 @EnableScheduling
 @SpringBootApplication
