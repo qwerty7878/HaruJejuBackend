784a3c5695ff0a3fe96e10e63c47c885ee668003
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-01-19 08:33:57 +0900

    Add EntityScan annotation for entity classes

--- jejuday/src/main/java/com/goodda/jejuday/JejudayApplication.java
+++ jejuday/src/main/java/com/goodda/jejuday/JejudayApplication.java
@@ -3,9 +3,11 @@ package com.goodda.jejuday;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EnableScheduling
 @SpringBootApplication
+@EntityScan(basePackageClasses={User.class, Spot.class, SpotViewLog.class})
 public class JejudayApplication {
 
     public static void main(String[] args) {
